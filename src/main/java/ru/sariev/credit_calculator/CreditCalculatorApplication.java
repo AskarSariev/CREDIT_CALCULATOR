b6066e6450c609122976c74d2cb6f4da4f06e700
Askar Sariev <askar-64@mail.ru>
2022-11-18 07:08:21 +0300

    Create project

---
+++ src/main/java/ru/sariev/credit_calculator/CreditCalculatorApplication.java
@@ -0,0 +1,13 @@
+package ru.sariev.credit_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sariev/credit_calculator/CreditCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sariev.credit_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
